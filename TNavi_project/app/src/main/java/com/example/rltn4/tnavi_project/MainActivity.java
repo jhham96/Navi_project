58270112d6cd02c1e0aba1d39d7be6c37b6b33cb
LDongGyu <ehdrbchd@naver.com>
2018-10-05 14:42:07 +0900

    파이어베이스 설치

--- TNavi_project/app/src/main/java/com/example/rltn4/tnavi_project/MainActivity.java
+++ TNavi_project/app/src/main/java/com/example/rltn4/tnavi_project/MainActivity.java
@@ -14,6 +14,12 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
