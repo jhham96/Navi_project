727d5722fd74e12fbf30880465496bc1f62c18c0
LDongGyu <ehdrbchd@naver.com>
2018-10-29 14:36:43 +0900

    dialog 추가

--- TNavi_project/app/src/main/java/com/example/rltn4/tnavi_project/GpsInfo.java
+++ TNavi_project/app/src/main/java/com/example/rltn4/tnavi_project/GpsInfo.java
@@ -1,6 +1,7 @@
 package com.example.rltn4.tnavi_project;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
@@ -252,6 +253,21 @@ public class GpsInfo extends Service implements LocationListener, Serializable {
                     textView.setText(messageList.get(mIndex));
                 } else {
                     textView.setText("도착하였습니다.");
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    builder.setTitle("도착하였습니다.");
+                    builder.setMessage("화면 종료하시겠습니까?");
+                    builder.setPositiveButton("예",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ((Activity)mContext).finish();
+                                }
+                            });
+                    builder.setNegativeButton("아니오",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            });
+                    builder.show();
                 }
             }
         }
